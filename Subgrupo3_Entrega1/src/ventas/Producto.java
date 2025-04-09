b71d0a0d4e1deab7c4b065cc9db4c27474f4c34d
Dianaporras06 <dianaporras659@gmail.com>
2025-04-08 22:42:27 -0500

    Update Producto10:41.java

--- Subgrupo3_Entrega1/src/ventas/Producto.java
+++ Subgrupo3_Entrega1/src/ventas/Producto.java
@@ -3,16 +3,17 @@ package ventas;
 public class Producto {
     private String id;
     private String nombre;
+    // Constructor que inicializa un producto con un ID y un nombre
 
     public Producto(String id, String nombre) {
         this.id = id;
         this.nombre = nombre;
     }
-
+// Getter para obtener el ID del producto
     public String getId() {
         return id;
     }
-
+// Getter para obtener el nombre del producto
     public String getNombre() {
         return nombre;
     }
